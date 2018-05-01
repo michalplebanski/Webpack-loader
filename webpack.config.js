8fb833845e48a083ef40af12541c396ff510a964
Michal <michalplebanski@gmail.com>
2018-05-01 10:54:51 +0200

    adding separate enviorments

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-        output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader'},
-                    {
-                      loader: 'css-loader',
-                      options: {
-                          modules: true
+module.exports = (env) => {
+    const enviorment = env || 'production';
+
+    return {
+        mode: enviorment,
+        entry: './src/index.js',
+            output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + enviorment + '.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader'},
+                        {
+                          loader: 'css-loader',
+                          options: {
+                              modules: true
+                            }
                         }
-                    }
-                ]
-            }
-        ]
+                    ]
+                }
+            ]
+        }
     }
 };
